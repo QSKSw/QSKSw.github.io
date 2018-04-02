402cc4f9a034dadd292c459bd9c1f707ac7a77a7
QSKSw <josefina.vineyard@gmail.com>
2018-04-01 23:26:34 -0400

    Bug Queen
    HL3

---
+++ games/HL3/HL3.bat
@@ -0,0 +1,33 @@
+@ECHO off
+ECHO	                                   ..........                                   
+ECHO	                            .,,;;,,!;!.;; ,..;;                                 
+ECHO	                          .,;;;!!;!!!;,vvv!;,.;;. ..                            
+ECHO	                        ,,!vLF$FLLE$FFl!!!Llv!.,;;,,,.                          
+ECHO	                      ,;!vlFLFFEE$$$$$ELvvlF$l!!;;!!,.,.                        
+ECHO	                     .,;LlLLE$LLE$$$$$$$EFEEvvl!!!vv;,,.                        
+ECHO	                     .!!vllEE$$$$$$$$$$$$EFllvv!;;lL,,,..                       
+ECHO	                    .,F!,,;vLE$E$$$$EEFFlv!!;.    ,;;,...                       
+ECHO	                     ,v;  .,,;;;;;;;;;;;,,,...     ,...,,.                      
+ECHO	                     ,!.  ..,,,,,....,,,......      .,..;.                      
+ECHO	                      ;,  ....................       ..,;;.                     
+ECHO	                     .!,  ...................         ..!; ..                   
+ECHO	                      ,,     ........,,....  ..,.     ,;,..                     
+ECHO	                       ...,,;;,,,,,,,,,,;!vLLFFLlv!;,;., ..                     
+ECHO	                       vv!!!!lElllLLllLFlvv;,!,.. !$.                           
+ECHO	                       v!  .,;;;;,.$;,vE.,,;;,,.   E                            
+ECHO	                        v  APRIL  l! . ,l;  FOOLS  ;;                            
+ECHO	                         ,,,,,,,;;,  ....,;;;;,,,.                              
+ECHO	                               ..,,  .... .,......     vLl!,                    
+ECHO	                               ...,,...,;!;,,.  . .    l$ELFELv.                
+ECHO	                            ;....;!vllFLLFLLvv!;....  ;l$$FllLEEv               
+ECHO	                         ,vLFL,.!!,,;;!!!!;;!;!ll;  ,!!vL$$ELlLEl               
+ECHO	                        ;vlLFLl..;,,,vlllLv!,,!v!, !v!vvvL$$FLEL......          
+ECHO	                       ;!vv!!vLL;,v!;v$$$EL!!lL!.,lllv!!vv$$FEv..;;,..          
+ECHO	                    .  !!!;!vlLEF!,,llFF$FLvll!,lFLllLlvlF$$l,,!v!;,,...        
+ECHO	                   ., .!!;;;!vF$EFFLvllLLlvvvvlLFEFFFFFEE$L;;!lvv!!;;;,,,.......
+ECHO	                ..,;; .!;;,;;!lEEFlLElFEELLllLLL$E$$$$EEF!,;vlvv!!!;;;,,;;;;;,,.
+ECHO	       ..,;;,..,,;!!; .v!;;;,;!vFEvLEllEELllllF$$$EFLLl;.;vvvvv!!!!!!!;vv!!!;;;,
+ECHO	  ..,,;!vvvv!;;;!!!!!. v!!!!;;,;vFElLFlFEFLLE$$EFllv;,.;vvv!!!!v!!!!!!lvvvv!!!!;
+ECHO	 ,;!vvvvvvvvv!!vv!!!v; ;vv!!!!!!!!vvvvvvvvlllllll;,.,!!!!!!!!!!!!!!!vlvvlvvv!!!;
+ECHO	.;;!!!!!vvvvlvvvv!v!vv,,v;;;;;;;;,,,,,,,,,,,,,,,;,,;;;;;;;;;;!;;!!!llllvvvv!!!!;	
+pause	
