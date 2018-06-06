e991b6687362284291d59c862f75fe95e76c9c3f
QSKSw <josefina.vineyard@gmail.com>
2018-06-06 00:25:17 -0400

    bees
    just gotta have bees

--- tampermonkey/bionicle-wiki-break-glass.user.js
+++ tampermonkey/bionicle-wiki-break-glass.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Incase of Bionicle Wiki, Break Glass
-// @namespace    bionicle.wikia.com*
-// @version      0.1
+// @namespace    bionicle.wikia.com/*
+// @version      0.2
 // @author       QSKSw
-// @match        http://*/*
+// @match        http://bionicle.wikia.com/*
 // @grant        none
+// @include      bionicle.wikia.com*
+// @exclude
 // ==/UserScript==
 
 if (window.confirm("DON'T PANIC! Just click this OK and you'll be taken to the BioSector01 version of this page, you unclean swine.")) {
