691a16c36b00b21c61ead1c5d239521c46524b58
QSKSw <josefina.vineyard@gmail.com>
2018-11-17 14:17:42 -0500

    crap
    crap

--- tampermonkey/bionicle-wiki-break-glass.user.js
+++ tampermonkey/bionicle-wiki-break-glass.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Incase of Bionicle Wiki, Break Glass
 // @namespace    bionicle.fandom.com/*
-// @version      0.5
+// @version      0.6
 // @author       QSKSw
 // @match        http://bionicle.fandom.com/*
 // @match        https://bionicle.fandom.com/*
@@ -11,6 +11,6 @@
 // ==/UserScript==
 
 if (window.confirm("DON'T PANIC! Just click this OK and you'll be taken to the BioSector01 version of this page, you unclean swine.")) {
-  javascript:if(location.hostname=="bionicle.fandom.com") {location.hostname="biosector01.com"};
+  javascript:if(location.hostname=="bionicle.fandom.com/wiki") {location.hostname="biosector01.com"};
 }
 
