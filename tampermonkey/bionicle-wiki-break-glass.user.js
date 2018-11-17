afa7f9c152b6b075c9c74ca94f5a64e4e8e4f4e5
QSKSw <josefina.vineyard@gmail.com>
2018-11-17 14:13:07 -0500

    wikia
    wikia

--- tampermonkey/bionicle-wiki-break-glass.user.js
+++ tampermonkey/bionicle-wiki-break-glass.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name         Incase of Bionicle Wiki, Break Glass
-// @namespace    bionicle.wikia.com/*
-// @version      0.3
+// @namespace    bionicle.fandom.com/*
+// @version      0.4
 // @author       QSKSw
 // @match        http://bionicle.wikia.com/*
 // @match        https://bionicle.wikia.com/*
+// @match        http://bionicle.fandom.com/*
+// @match        https://bionicle.fandom.com/*
 // @grant        none
 // @include      bionicle.wikia.com*
 // @exclude
 // ==/UserScript==
 
 if (window.confirm("DON'T PANIC! Just click this OK and you'll be taken to the BioSector01 version of this page, you unclean swine.")) {
-  javascript:if(location.hostname=="bionicle.wikia.com") {location.hostname="biosector01.com"};
+  javascript:if(location.hostname=="bionicle.fandom.com") {location.hostname="biosector01.com"};
 }
 
