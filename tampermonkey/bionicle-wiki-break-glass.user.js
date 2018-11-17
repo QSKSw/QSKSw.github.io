69b2d3db41637f7fdbe55a5641574141615a1dd7
QSKSw <josefina.vineyard@gmail.com>
2018-11-17 14:24:51 -0500

    why am i so bad at this
    why am i so bad at this

--- tampermonkey/bionicle-wiki-break-glass.user.js
+++ tampermonkey/bionicle-wiki-break-glass.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         Incase of Bionicle Wiki, Break Glass
 // @namespace    bionicle.fandom.com/*
-// @version      0.7
+// @version      0.8
 // @author       QSKSw
 // @match        http://bionicle.fandom.com/*
 // @match        https://bionicle.fandom.com/*
 // @grant        none
-// @include      bionicle.wikia.com*
+// @include      bionicle.fandom.com*
 // @exclude
 // ==/UserScript==
 
 if (window.confirm("DON'T PANIC! Just click this OK and you'll be taken to the BioSector01 version of this page, you unclean swine.")) {
-  
-  var url = window.location.toString();
-window.location = url.replace(bionicle.fandom.com/wiki, 'biosector01.com');
+
+ location.href = location.href.replace(
+    'bionicle.fandom.com', 'biosector01.com')
   
 }
 
