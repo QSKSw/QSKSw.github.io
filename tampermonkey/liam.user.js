d994f1120d90d76d9b1a7e698874ee2404354eb2
QSKSw <josefina.vineyard@gmail.com>
2019-01-11 03:17:11 -0500

    why am i bad
    why am i bad

--- tampermonkey/liam.user.js
+++ tampermonkey/liam.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name        Liam
+// @name        Liam is Google
 // @author      QSKSw
+// @description Liam is Google
 // @namespace   http://*.google.com/*
 // @namespace   https://*.google.com/*
-// @version     0.1
+// @version     0.2
 // @include     http://*.google.com/*
 // @include     https://*.google.com/*
 // ==/UserScript==
 
-document.getElementById("hplogo").innerHTML = "<a href=https://www.youtube.com/user/Vahkiti/ alt=Butts title=Butts><img alt=Butts title=Butts height=92 id=hplogo src=http://github.github.io/tampermonkey/Liam.PNG srcset=http://github.github.io/tampermonkey/Liam.PNG 1x, http://github.github.io/tampermonkey/Liam.PNG 2x style=padding-top:109px width=272 onload=window.lol&amp;&amp;lol()></a>";
+document.getElementById("hplogo").innerHTML = "<a href=https://www.youtube.com/user/Vahkiti/ alt=Butts title=Butts><img alt=Butts title=Butts height=92 id=hplogo src=http://qsksw.github.io/tampermonkey/Liam.PNG srcset=http://qsksw.github.io/tampermonkey/Liam.PNG 1x, http://qsksw.github.io/tampermonkey/Liam.PNG 2x style=padding-top:109px width=272 onload=window.lol&amp;&amp;lol()></a>";
