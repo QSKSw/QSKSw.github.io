dc921f7565f1b9c7c494e95f661680361c6f144a
QSKSw <josefina.vineyard@gmail.com>
2019-01-11 03:19:48 -0500

    what even is life
    what even is life

--- tampermonkey/liam.user.js
+++ tampermonkey/liam.user.js
@@ -4,9 +4,9 @@
 // @description Liam is Google
 // @namespace   http://*.google.com/*
 // @namespace   https://*.google.com/*
-// @version     0.2
+// @version     0.3
 // @include     http://*.google.com/*
 // @include     https://*.google.com/*
 // ==/UserScript==
 
-document.getElementById("hplogo").innerHTML = "<a href=https://www.youtube.com/user/Vahkiti/ alt=Butts title=Butts><img alt=Butts title=Butts height=92 id=hplogo src=http://qsksw.github.io/tampermonkey/Liam.PNG srcset=http://qsksw.github.io/tampermonkey/Liam.PNG 1x, http://qsksw.github.io/tampermonkey/Liam.PNG 2x style=padding-top:109px width=272 onload=window.lol&amp;&amp;lol()></a>";
+document.getElementById("hplogo").innerHTML = "<a href=https://www.youtube.com/user/Vahkiti/ alt=Butts title=Butts><img alt=Butts title=Butts height=92 id=hplogo src=http://qsksw.github.io/tampermonkey/Liam.png srcset=http://qsksw.github.io/tampermonkey/Liam.png 1x, http://qsksw.github.io/tampermonkey/Liam.png 2x style=padding-top:109px width=272 onload=window.lol&amp;&amp;lol()></a>";
