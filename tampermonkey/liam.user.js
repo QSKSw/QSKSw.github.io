75b77fc33213d3bb38398fe6f72085b42d2f4ab7
QSKSw <josefina.vineyard@gmail.com>
2019-01-11 03:25:48 -0500

    WHY
    WHY

--- tampermonkey/liam.user.js
+++ tampermonkey/liam.user.js
@@ -4,9 +4,9 @@
 // @description Liam is Google
 // @namespace   http://*.google.com/*
 // @namespace   https://*.google.com/*
-// @version     0.4
+// @version     0.5
 // @include     http://*.google.com/*
 // @include     https://*.google.com/*
 // ==/UserScript==
 
-document.getElementById("hplogo").innerHTML = "<a href=https://www.youtube.com/user/Vahkiti/ alt=Butts title=Butts><img alt=Butts title=Butts height=92 id=hplogo src=http://qsksw.github.io/tampermonkey/liam.png srcset=http://qsksw.github.io/tampermonkey/liam.png 1x, http://qsksw.github.io/tampermonkey/liam.png 2x style=padding-top:109px width=272 onload=window.lol&amp;&amp;lol()></a>";
+document.getElementById("hplogo").innerHTML = "<a href=https://www.youtube.com/user/Vahkiti/ alt=Butts title=Butts><img alt=Butts title=Butts height=92 id=hplogo src=https://qsksw.github.io/tampermonkey/liam.png srcset=https://qsksw.github.io/tampermonkey/liam.png 1x, https://qsksw.github.io/tampermonkey/liam.png 2x style=padding-top:109px width=272 onload=window.lol&amp;&amp;lol()></a>";
