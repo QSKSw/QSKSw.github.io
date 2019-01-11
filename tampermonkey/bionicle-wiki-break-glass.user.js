df248bd27aa6d8df336213f92ca1c62dfe39bbeb
QSKSw <josefina.vineyard@gmail.com>
2019-01-11 03:06:39 -0500

    test1
    test1

--- tampermonkey/bionicle-wiki-break-glass.user.js
+++ tampermonkey/bionicle-wiki-break-glass.user.js
@@ -1,7 +1,10 @@
 // ==UserScript==
 // @name         Incase of Bionicle Wiki, Break Glass
 // @namespace    bionicle.fandom.com/*
-// @version      0.8
+// @version      0.9
+
+// @description  If you find yourself on the Bionicle Wiki, DON'T PANIC! We'll bring up a window warning you whenever you find yourself sinning; just click "OK" and you'll instantly be taken to the BioSector01 version of whatever blasphemous page you happen to be on, you unclean swine.
+
 // @author       QSKSw
 // @match        http://bionicle.fandom.com/*
 // @match        https://bionicle.fandom.com/*
---
+++ tampermonkey/liam.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Liam
+// @author      QSKSw
+// @namespace   http://*.google.com/*
+// @namespace   https://*.google.com/*
+// @version     0.1
+// @include     http://*.google.com/*
+// @include     https://*.google.com/*
+// ==/UserScript==
+
+document.getElementById("hplogo").innerHTML = "<a href=https://www.youtube.com/user/Vahkiti/ alt=Butts title=Butts><img alt=Butts title=Butts height=92 id=hplogo src=http://github.github.io/tampermonkey/Liam.PNG srcset=http://github.github.io/tampermonkey/Liam.PNG 1x, http://github.github.io/tampermonkey/Liam.PNG 2x style=padding-top:109px width=272 onload=window.lol&amp;&amp;lol()></a>";
