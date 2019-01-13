35fd7330cab03e91d5e0c2b33ece826009ce7f0f
QSKSw <josefina.vineyard@gmail.com>
2019-01-12 19:42:43 -0500

    Needed to update Liam
    Needed to update Liam

--- tampermonkey/liam.user.js
+++ tampermonkey/liam.user.js
@@ -4,9 +4,11 @@
 // @description Liam is Google
 // @namespace   http://*.google.com/*
 // @namespace   https://*.google.com/*
-// @version     0.5
+// @version     0.6
 // @include     http://*.google.com/*
 // @include     https://*.google.com/*
 // ==/UserScript==
+
+document.getElementById("lga").innerHTML = "<a href=https://www.youtube.com/user/Vahkiti/ alt=Butts title=Butts><img alt=Butts title=Butts height=92 id=hplogo src=https://qsksw.github.io/tampermonkey/liam.png srcset=https://qsksw.github.io/tampermonkey/liam.png 1x, https://qsksw.github.io/tampermonkey/liam.png 2x style=padding-top:109px width=272 onload=window.lol&amp;&amp;lol()></a>";
 
 document.getElementById("hplogo").innerHTML = "<a href=https://www.youtube.com/user/Vahkiti/ alt=Butts title=Butts><img alt=Butts title=Butts height=92 id=hplogo src=https://qsksw.github.io/tampermonkey/liam.png srcset=https://qsksw.github.io/tampermonkey/liam.png 1x, https://qsksw.github.io/tampermonkey/liam.png 2x style=padding-top:109px width=272 onload=window.lol&amp;&amp;lol()></a>";
