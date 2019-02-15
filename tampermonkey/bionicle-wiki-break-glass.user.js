03fb1a981ecbfe54203ee27f9df6c6a1d3e74bb5
QSKSw <josefina.vineyard@gmail.com>
2019-02-15 14:31:42 -0500

    This OK Button
    This OK Button

--- tampermonkey/bionicle-wiki-break-glass.user.js
+++ tampermonkey/bionicle-wiki-break-glass.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Incase of Bionicle Wiki, Break Glass
 // @namespace    bionicle.fandom.com/*
-// @version      0.9
+// @version      1.0
 
-// @description  If you find yourself on the Bionicle Wiki, DON'T PANIC! We'll bring up a window warning you whenever you find yourself sinning; just click "OK" and you'll instantly be taken to the BioSector01 version of whatever blasphemous page you happen to be on, you unclean swine.
+// @description  If you find yourself on the Bionicle Wiki, DON'T PANIC! We'll bring up a window warning you whenever you find yourself sinning; just click "OK" and you'll instantly be taken to the BioSector01 version of whatever blasphemous page you happen to be on, you unclean swine!
 
 // @author       QSKSw
 // @match        http://bionicle.fandom.com/*
@@ -13,7 +13,7 @@
 // @exclude
 // ==/UserScript==
 
-if (window.confirm("DON'T PANIC! Just click this OK and you'll be taken to the BioSector01 version of this page, you unclean swine.")) {
+if (window.confirm("DON'T PANIC! Just click that handy-dandy "OK" button and you'll be taken to the BioSector01 version of this page, you unclean swine!")) {
 
  location.href = location.href.replace(
     'bionicle.fandom.com', 'biosector01.com')
